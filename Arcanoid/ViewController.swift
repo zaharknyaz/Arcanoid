38dbb8b05ae740c02e7148844b886ddf9e38d4c2
Zahar Knyazev <knyazzz1989@mail.ru>
2021-12-30 00:00:25 +0400

    Arcanoid (rocket)

--- Arcanoid/ViewController.swift
+++ Arcanoid/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        addRocket()
+    }
+    
+    var viewRocket: UIView!
+    func addRocket() {
+        //создаем view(ракетку) программно
+        viewRocket = UIView(frame: CGRect(x: 30, y: UIScreen.main.bounds.size.height - 100, width: 150, height: 30))
+        viewRocket.backgroundColor = UIColor.orange
+        //добавим ракетку на основную view
+        view.addSubview(viewRocket)
+        
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(self.pan))
+        viewRocket.gestureRecognizers = [pan]
+    }
 
+    var centerRocket: CGPoint!
+    @objc
+    func pan(pgr: UIPanGestureRecognizer) {
+        if pgr.state == .began {
+            centerRocket = viewRocket.center
+        }
+        let x = pgr.translation(in: view).x
+        let newCenter = CGPoint(x: centerRocket.x + x, y: centerRocket.y)
+        viewRocket.center = newCenter
+    }
 }
 
