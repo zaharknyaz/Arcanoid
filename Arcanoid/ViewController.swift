ab45e0e17f236c3b913bb56839fd481a2810514a
Zahar Knyazev <knyazzz1989@mail.ru>
2021-12-30 23:57:25 +0400

    Arcanoid rocket + ball + beat

--- Arcanoid/ViewController.swift
+++ Arcanoid/ViewController.swift
@@ -61,9 +61,13 @@ class ViewController: UIViewController {
         var viewBall: UIView
         var viewParent: UIView
         
+        var viewRocket: UIView
+        
         //создаем шарик в parentView
-        init(in viewParent: UIView) {
+        init(in viewParent: UIView, viewRocket: UIView) {
             self.viewParent = viewParent
+            self.viewRocket = viewRocket
+            
             center = viewParent.center
             vector = Vector(a: CGPoint(x: 0, y: 0), b: CGPoint(x: 5, y: 5))
             viewBall = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
@@ -75,6 +79,40 @@ class ViewController: UIViewController {
         //функция определяет, где в следующий момент окажется шарик
         mutating func tic() {
             let newCenter = CGPoint(x: center.x + vector.dx, y: center.y + vector.dy)
+           
+            //столкновение шарика с ракеткой
+            //левая боковая грань
+            if center.x < viewRocket.frame.origin.x &&
+            newCenter.x >= viewRocket.frame.origin.x &&
+            newCenter.y >= viewRocket.frame.origin.y &&
+            newCenter.y <= viewRocket.frame.origin.y + viewRocket.frame.size.height {
+                vector.b.x = -vector.b.x
+            }
+            
+            //правая боковая грань
+            if center.x > viewRocket.frame.origin.x + viewRocket.frame.size.width &&
+            newCenter.x <= viewRocket.frame.origin.x + viewRocket.frame.size.width &&
+            newCenter.y >= viewRocket.frame.origin.y &&
+            newCenter.y <= viewRocket.frame.origin.y + viewRocket.frame.size.height {
+                vector.b.x = -vector.b.x
+            }
+            
+            //верхняя грань
+            if center.y < viewRocket.frame.origin.y &&
+            newCenter.y >= viewRocket.frame.origin.y &&
+            newCenter.x >= viewRocket.frame.origin.x &&
+            newCenter.x <= viewRocket.frame.origin.x + viewRocket.frame.size.width {
+                vector.b.y = -vector.b.y
+            }
+            
+            //нижняя грань
+            if center.y > viewRocket.frame.origin.y + viewRocket.frame.size.height &&
+            newCenter.y <= viewRocket.frame.origin.y + viewRocket.frame.size.height &&
+            newCenter.x >= viewRocket.frame.origin.x &&
+            newCenter.x <= viewRocket.frame.origin.x + viewRocket.frame.size.width {
+                vector.b.y = -vector.b.y
+            }
+            
             center = newCenter
             viewBall.center = newCenter
             
@@ -92,7 +130,7 @@ class ViewController: UIViewController {
     
     var ball: Ball!
     func addBall() {
-        ball = Ball(in: self.view)
+        ball = Ball(in: self.view, viewRocket: viewRocket)
     }
     
     func start() {
